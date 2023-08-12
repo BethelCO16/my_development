4bfcd84d8210d0f2df1d50382c580fcdb091b917
BethelCO16 <138235830+BethelCO16@users.noreply.github.com>
2023-08-12 08:29:08 +0100

    personal development files upload

---
+++ Personal Development/helloworld.py
@@ -0,0 +1,46 @@
+#This is at the top of my file
+print('Hello world - I am loving my life!')#This follows a line of code
+print('Bethel Onyealilachi')
+print(3)
+
+#This is a single line comment
+#To include a multiline comment
+#That spams multiple comments
+
+"""
+A multiline comment 
+and it will not show on the output
+"""
+
+
+
+'''
+This also works as a multiline comment and I can use it
+in my python file
+'''
+print('This is cool')
+
+customer="Kate"
+product='Iphone 11'
+price= "1000"
+height= 5.3j
+
+
+float_val=float(price)
+
+print(type(height))
+print(type(price))
+
+"""
+int() - Any value to an integer
+float() - Any value to a decimal or floating point number
+str() - Any value to a string
+
+"""
+
+print("Customer name:"+ customer)
+print("Product name:"+ product)
+print("Price:"+ str(price))
+print("Height:", height)
+print("Price", float_val)
+
